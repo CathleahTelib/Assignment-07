f6054cd04598b407477c1fbf2e23d5b616506c48
Cathleah Tabaquero <92615682+CathleahTelib@users.noreply.github.com>
2021-12-07 08:44:58 +0700

    Update W,V,C.py

--- W,V,C.py
+++ W,V,C.py
@@ -1,14 +1,13 @@
-
 sentence = input("Enter any sentence you want:")
 string= sentence.lower()
 vowels = 0
 consonants = 0
 words = 1
 
-for i in range(len(sentence)):
+for i in range(len(string)):
     if(sentence[i] == ' ' or sentence == '\n' or sentence == '\t'):
         words = words + 1
-for c in sentence:
+for c in string:
     if c in 'aeiou':
         vowels= vowels+1
     else:
